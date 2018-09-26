4752ec86d9267a3903a1bd4c07561e2188382952
Kwun Yeung <kwun@forbole.com>
2018-09-26 14:45:33 +0800

    try to add sidebar

--- imports/ui/blocks/BlocksTable.jsx
+++ imports/ui/blocks/BlocksTable.jsx
@@ -3,15 +3,28 @@ import { Meteor } from 'meteor/meteor';
 import { Table, Badge, Button } from 'reactstrap';
 import HeaderRecord from '/imports/ui/components/HeaderRecord.jsx';
 import Blocks from '/imports/ui/blocks/ListContainer.js'
+import Sidebar from "react-sidebar";
 
 class BlocksTable extends Component {
     constructor(props){
         super(props);
         this.state = {
             limit: 30,
+            sidebarOpen: false
         };
 
-        this.updateLimit.bind(this);
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+        this.showBlock = this.showBlock.bind(this);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
+    showBlock(e){
+        console.log("clicked");
+        console.log(e.target.value);
+        onSetSidebarOpen(true);
     }
 
     // TODO: update list
@@ -25,11 +38,21 @@ class BlocksTable extends Component {
     render(){
         return <div>
             <h1>Lastest blocks <Badge color="primary">{Meteor.settings.public.chainId}</Badge></h1>
-            <Table id="block-table">
-                <HeaderRecord />
-                <tbody id="blocks"><Blocks limit={this.state.limit} /></tbody>
+            
+                <Table id="block-table">
+                    <HeaderRecord />
+                    <tbody id="blocks"><Blocks limit={this.state.limit} onClick={this.showBlock}/></tbody>
                 
             </Table>
+            <Sidebar
+                sidebar={<b>Block</b>}
+                open={this.state.sidebarOpen}
+                onSetOpen={this.onSetSidebarOpen}
+                styles={{ sidebar: { background: "white" } }}
+                pullRight={true}
+            >
+                <b>Main content</b>
+            </Sidebar>
             {/* <Button color="primary" onClick={this.updateLimit}>Load more</Button>{' '} */}
             {/* <div id="loading" className="loader"></div> */}
         </div>
